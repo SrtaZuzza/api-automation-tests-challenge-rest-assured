31b2bea736d86d935d38f18fac66432c564fda61
srtazuzza <monicavc@ymail.com>
2022-12-01 21:34:53 -0300

    more tests

--- src/test/java/BookingTests.java
+++ src/test/java/BookingTests.java
@@ -16,6 +16,11 @@ import static io.restassured.config.LogConfig.logConfig;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
 import static org.hamcrest.Matchers.*;
 
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 public class BookingTests {
     public static Faker faker;
     private static RequestSpecification request;
@@ -34,7 +39,8 @@ public class BookingTests {
                 faker.internet().password(8,10),
                 faker.phoneNumber().toString());
 
-        bookingDates = new BookingDates("2018-01-02", "2018-01-03");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        bookingDates = new BookingDates(sdf.format(faker.date().past(1, TimeUnit.DAYS)), sdf.format(faker.date().future(1, TimeUnit.DAYS)));
         booking = new Booking(user.getFirstName(), user.getLastName(),
                 (float)faker.number().randomDouble(2, 50, 100000),
                 true,bookingDates,
@@ -46,56 +52,123 @@ public class BookingTests {
     void setRequest(){
         request = given().config(RestAssured.config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
                 .contentType(ContentType.JSON)
-                .auth().basic("admin", "password123");
+                .auth().basic("admin", "password123")
+        ;
     }
 
-    @Test
-    public void getAllBookingsById_returnOk(){
-            Response response = request
-                                    .when()
-                                        .get("/booking")
-                                    .then()
-                                        .extract()
-                                        .response();
+    @Test // Create booking
+    public void  CreateBooking_WithValidData_returnOk(){
+        request
+            .contentType(ContentType.JSON)
+            .when()
+                .body(booking)
+                .post("/booking")
+            .then()
+                .body(matchesJsonSchemaInClasspath("createBookingRequestSchema.json"))
+            .and()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON).and().time(lessThan(2000L))
+            .extract()
+                .jsonPath()
+        ;
+    }
 
+    @Test // create token
+    public void CreateAuthToken(){
+        Map<String, String> body = new HashMap<>();
+        body.put("username", "admin");
+        body.put("password", "password123");
+
+        request
+            .contentType(ContentType.JSON)
+            .when()
+                .body(body)
+                .post("/auth")
+            .then()
+                .assertThat()
+                .statusCode(200)
+            .extract()
+                .path("token")
+        ; 
+    }
+
+    @Test // Get Booking id list
+    public void getAllBookingIds_returnOk(){
+        Response response = request
+            .when()
+                .get("/booking")
+            .then()
+                .extract()
+                .response()
+        ;
 
         Assertions.assertNotNull(response);
         Assertions.assertEquals(200, response.statusCode());
     }
 
+    @Test // Get booking by id
+    public void getBookingById_returnOk(){
+        request
+            .when()
+                .get("/booking/" + 7771)
+            .then()
+                .assertThat()
+                .statusCode(200)
+        ;
+    }
+
     @Test
     public void  getAllBookingsByUserFirstName_BookingExists_returnOk(){
-                    request
-                        .when()
-                            .queryParam("firstName", "Carol")
-                            .get("/booking")
-                        .then()
-                            .assertThat()
-                            .statusCode(200)
-                            .contentType(ContentType.JSON)
-                        .and()
-                        .body("results", hasSize(greaterThan(0)));
-
+        request
+            .when()
+                .queryParam("firstName", faker.name().firstName())
+                .get("/booking")
+            .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+            .and()
+                .body("results", hasSize(greaterThan(0)))
+        ;
     }
 
     @Test
-    public void  CreateBooking_WithValidData_returnOk(){
-
-        Booking test = booking;
-        given().config(RestAssured.config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
-                    .contentType(ContentType.JSON)
-                        .when()
-                        .body(booking)
-                        .post("/booking")
-                        .then()
-                        .body(matchesJsonSchemaInClasspath("createBookingRequestSchema.json"))
-                        .and()
-                        .assertThat()
-                        .statusCode(200)
-                        .contentType(ContentType.JSON).and().time(lessThan(2000L));
-
-
-
+    public void  getAllBookingsByPrice_BookingExists_returnOk(){
+        request
+            .when()
+                .queryParam("totalprice", 1000)
+                .get("/booking")
+            .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+            .and()
+                .body("results", hasSize(greaterThan(0)))
+        ;
     }
 
+    @Test // Delete booking
+    public void DeleteBookingById_returnOk(){
+        request
+            //.header("Authorization","Basic")
+            .header("Cookie", "token=3390c953ce6c173")
+            .when()
+                .delete("/booking/" + faker.number().digits(5))
+            .then()
+                .assertThat()
+                .statusCode(201)
+        ;
+    }
+
+    @Test // Health check
+    public void ApiIsUpCheck_returnCreated(){
+        request
+            .when()
+                .get("/ping")
+            .then()
+                .assertThat()
+                .statusCode(201)
+        ;
+    }
 }
